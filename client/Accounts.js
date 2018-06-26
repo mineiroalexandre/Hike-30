bf24f28e5dbe19ccc97371ef965d4b5e1d76fe31
Alexandre Mineiro <mineiro.alexandre.a@gmail.com>
2018-06-26 12:09:15 -0300

    Implementado login usando useraccounts

---
+++ client/Accounts.js
@@ -0,0 +1,39 @@
+AccountsTemplates.configure({
+  defaultLayoutType: 'blaze', // Optional, the default is 'blaze'
+  defaultLayout: 'LayoutPrincipal',
+  defaultLayoutRegions: {
+      nav: 'Cabecalho',
+  },
+  defaultContentRegion: 'main'
+});
+
+AccountsTemplates.configureRoute('signIn');
+
+T9n.setLanguage('pt');
+
+let pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+  _id: 'username',
+  type: "text",
+  displayName: 'username',
+  required: true,
+  minLength: 5,
+  },
+  {
+  _id: 'email',
+  type: 'email',
+  required: true,
+  displayName: 'email',
+  re: /.+@(.+){2,}\.(.+){2,}/,
+  errStr: 'Invalid email',
+  },
+  pwd,
+  {
+	_id: 'name',
+  type: 'text',
+  displayName: 'Nome Completo',
+  required: true,
+  }
+]);
