a4342248684e7d83be9aac9b5ed841461ff71c1b
Alexandre Mineiro <mineiro.alexandre.a@gmail.com>
2018-06-28 16:23:30 -0300

    Inplementar sistema de login, associar ele aos posts e aos sistema de curtir e descurtir posts.

---
+++ client/Feed/Post.js
@@ -0,0 +1,32 @@
+Template.Post.helpers({
+	usernameDoAutor: function() {
+		let idDoAutor = this.idDoAutor;
+		let autor = Meteor.users.findOne({_id: idDoAutor})
+		return autor.username;
+	},
+	numeroDeCurtidas: function() {
+		return this.curtidas.length;
+	},
+	usuarioCurtiu: function() {
+		let curtidas = this.curtidas;
+
+		let posicao = curtidas.indexOf(Meteor.userId());
+
+		if(posicao === -1) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+});
+
+Template.Post.events({
+	"click .like-button": function(event, template) {
+		Meteor.call("curtirPost", template.data._id);
+
+	},
+	"click .dislike-button": function(event, template) {
+		Meteor.call("descurtirPost", template.data._id);
+
+	}
+});
